9d28d6587fbefe333d7475ec2e50765213069eb4
Martin Olivares <martin.olivarest@utem.cl>
2025-10-23 22:16:51 -0300

    feat: update version code and version name; modify Google client configuration with new certificate hashes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.spikai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
